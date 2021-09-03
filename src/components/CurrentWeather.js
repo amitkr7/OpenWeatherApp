12862e1387ef2a66b6c02578f3c5df7dc35e2c7a
Amit <amit840901@gmail.com>
2021-09-03 19:15:06 +0530

    CurrentWeather Component

---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const CurrentWeather = ({ currentTemp, humidity, pressure, windSpeed }) => {
+  return (
+    <div className='card'>
+      <div className='center'>
+        <h3>Current Weather</h3>
+      </div>
+
+      <div className='right'>
+        Temperature
+        <i className='fa fa-temperature-high'></i>
+        <span>{currentTemp} °C</span>
+      </div>
+      <div className='right'>
+        Humidity
+        <i className='fas fa-cloud-sun-rain'></i>
+        <span>{humidity} </span>
+      </div>
+      <div className='right'>
+        Pressure
+        <i className='fas fa-compress-arrows-alt'></i>
+        <span>{pressure} </span>
+      </div>
+      <div className='right'>
+        WindSpeed
+        <i className='fas fa-wind'></i>
+        <span>{windSpeed} </span>
+      </div>
+    </div>
+  )
+}
+
+export default CurrentWeather
